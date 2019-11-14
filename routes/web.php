4175de12f4d185bb1f97dba2b5223fd336b0c1da
TARCISIO NUNES <tarcnux@gmail.com>
2019-11-13 23:37:15 -0300

    cruds

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,14 @@
 
 
 Route::get('/','indexController@index');
+Route::get('/cronometragem','CronometragemController@index');
+
+//rotas crud
+Route::resource('operacao','OperacaoController');
+Route::resource('usuario','UsuarioController');
+Route::resource('produto','ProdutoController');
+Route::resource('sequencia','SequenciaController');
+Route::resource('tomada-Tempo','TomadaTempoController');
+
+
+

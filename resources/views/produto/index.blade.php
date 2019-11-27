9e8c0428731bb2591089482169e91e467ce94976
albor95 <47107868+albor95@users.noreply.github.com>
2019-11-26 22:40:03 -0300

    Update index.blade.php

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -1,84 +1,83 @@
 @extends('layout')
 @section('conteudo')
- 
- <section id="main-content">
-                    <section class="wrapper">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <h3 class="page-header"><i class="fa fa-table"></i> Listar Produtos</h3>
-                                <ol class="breadcrumb">
-                                    <li><i class="fa fa-home"></i><a href="index.html">Início</a></li>
-                                    <li><i class="fa fa-table"></i>Produtos</li>
-                                    <li><i class="fa fa-th-list"></i>Listar Produtos</li>
-                                </ol>
-                            </div>
-                        </div>
-                        <!-- page start-->
 
+<script>
 
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <section class="panel">
-                                    <header class="panel-heading">
-                                        Produtos Cadastrados
-                                    </header>
+    function delProduto(id, url){
+    confirma=confirm("keh memu faze issu?");
+    if(confirma){
+    $.noConflict();
+    dados = $('#' + id).serialize();
+    
+    $.ajax({
+    method:'post',
+            url:url,
+            data:dados,
+            dataType:'html',
+            success: function (data){
+            $('#linha'+id).remove();
+            },
+            error:function (argument){
+            alert('Usuário ta la ainda ');
+            }
+    });  
+    }
+    return false;
+    }
 
-                                    <table class="table table-striped table-advance table-hover">
-                                        <tbody>
-                                            <tr>
-                                                <th><i class="icon_id-2"></i>Código</th>
-                                                <th><i class="icon_pens"></i>Nome</th>
-                                                <th><i class="icon-task-l"></i>Operações</th>
-                                            </tr>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>Camisa Xadrez</td>
-                                                <td>
-                                                    <a id="btn_add_op" class="btn btn-primary" href="cadastro_operacao.html"><i class="icon_plus_alt2"></i> Adicionar</a>
-                                                    <a id="btn_lista_op" class="btn btn-secondary" onclick="abrirModal();"><i class=""></i> Listar</a>
-                                                </td>
-                                                <td>
-                                                    <div class="btn-group">
-                                                        <a class="btn btn-secondary" href="#"><i class="icon_pencil-edit"></i> Editar</a>
-                                                        <a class="btn btn-danger" href="#"><i class="icon_close_alt2"></i></a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>2</td>
-                                                <td>Cachecol Tri</td>
-                                                <td>
-                                                    <a id="btn_add_op" class="btn btn-primary" href="cadastro_operacao.html"><i class="icon_plus_alt2"></i> Adicionar</a>
-                                                    <a id="btn_lista_op" class="btn btn-secondary" onclick="abrirModal();"><i class=""></i> Listar</a>
-                                                </td>
-                                                <td>
-                                                    <div class="btn-group">
-                                                        <a class="btn btn-secondary" href="#"><i class="icon_pencil-edit"></i> Editar</a>
-                                                        <a class="btn btn-danger" href="#"><i class="icon_close_alt2"></i></a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</td>
-                                                <td>Calça Moletom</td>
-                                                <td>
-                                                    <a id="btn_add_op" class="btn btn-primary" href="cadastro_operacao.html"><i class="icon_plus_alt2"></i> Adicionar</a>
-                                                    <a id="btn_lista_op" class="btn btn-secondary" onclick="abrirModal();"><i class=""></i> Listar</a>
-                                                </td>
-                                                <td>
-                                                    <div class="btn-group">
-                                                        <a class="btn btn-secondary" href="#"><i class="icon_pencil-edit"></i> Editar</a>
-                                                        <a class="btn btn-danger" href="#"><i class="icon_close_alt2"></i></a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </section>
-                            </div>
-                        </div>
-                        <!-- page end-->
-                    </section>
+</script>
+<!--main content start-->
+<section id="main-content">
+    <section class="wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h3 class="page-header"><i class="fa fa-table"></i>Lista de Produtos</h3>
+                <ol class="breadcrumb">
+                    
+
+                </ol>
+            </div>
+        </div>
+        <!-- page start-->
+
+        <div class="row">
+            <div class="col-lg-12">
+                <section class="panel">
+
+                    <table class="table table-striped table-advance table-hover">
+                        <tbody>
+
+                            <tr>
+                                <th><i class="icon_pin"></i> Cod.</th>
+                                <th><i class="icon_tool"></i> Produto</th>
+                                <th><i class="icon_cogs"></i> Ação</th>
+                            </tr>
+                            @foreach($peodutos as $p)
+                            <tr id="linhadel{{$p->ProCod}}">
+                                 <td>{{$p->ProCod}}</td>  
+                                <td>{{$p->ProNom}}</td>  
+                               
+                                <td>
+                                    <div class="btn-group">
+                                        <a href="{{route('produto.edit', $p->ProCod)}}" class="btn btn-primary"><i class="icon_pencil-edit"></i> Editar</a>
+                                        <a onclick="return delProduto('del{{$p->ProCod}}','{{route('produto.destroy', $p->ProCod)}}')"class="btn btn-danger"><i class="icon_close_alt2" ></i> Deletar</a>
+
+                                        <form action="" method="post" id="del{{$p->ProCod}}">
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </section>
-            
+            </div>
+        </div>
+        <!-- page end-->
+    </section>
+</section>
+<!--main content end-->
+
 @stop
